2a53056e1d9d83e88bfdd268f5f67a500c711dd9
Samuel Adetunji <guysnacho2@gmail.com>
2021-01-14 17:45:29 -0600

    Listed text from item list

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/header'
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+  const [items, setitems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Cheese'},
+    {id: uuidv4(), text: 'Avocados'},
+    {id: uuidv4(), text: 'Kale'},
+    {id: uuidv4(), text: 'Irish Sea Moss'}
+  ])
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
+      <FlatList 
+      data={items}
+      renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   )
 }
