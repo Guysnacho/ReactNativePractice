0f0d4ad64d9fa8ee057c3810bd29f48f460071b0
Samuel Adetunji <guysnacho2@gmail.com>
2021-01-14 18:02:57 -0600

    Added removeItem functionallity

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
-import Header from './components/header'
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/header';
 import ListItem from './components/ListItem.js';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
-  const [items, setitems] = useState([
+  const [items, setItems] = useState([
     {id: uuidv4(), text: 'Milk'},
     {id: uuidv4(), text: 'Cheese'},
     {id: uuidv4(), text: 'Avocados'},
     {id: uuidv4(), text: 'Kale'},
-    {id: uuidv4(), text: 'Irish Sea Moss'}
-  ])
+    {id: uuidv4(), text: 'Irish Sea Moss'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Header title='Shopping List' />
-      <FlatList 
-      data={items}
-      renderItem={({item}) => <ListItem item = {item} />}
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
 
 export default App;
